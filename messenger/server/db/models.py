8e04475795f0611763530897bc882a0e5232f9ec
SPELLGIRL <chepushilka@yandex.ru>
2019-07-26 01:54:28 +0300

    Начата реализация БД для проекта.

---
+++ messenger/server/db/models.py
@@ -0,0 +1,49 @@
+import datetime
+from sqlalchemy import Column, Table, String, Integer, Boolean, ForeignKey, DateTime
+from sqlalchemy.orm import *
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+contact_table = Table(
+    'contact', Base.metadata,
+    Column('user_id', Integer, ForeignKey('user.id')),
+    Column('contact_id', Integer, ForeignKey('user.id'))
+)
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    last_login = Column(DateTime)
+    is_online = Column(Boolean)
+    history = relationship('History', back_populates='user')
+    contacts = relationship('User', secondary=contact_table,
+                            backref='owner',
+                            primaryjoin=id == contact_table.c.user_id,
+                            secondaryjoin=id == contact_table.c.contact_id
+                            )
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f'<Клиент {self.name}>'
+
+
+class History(Base):
+    __tablename__ = 'history'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    time = Column(DateTime)
+    ip = Column(String)
+    user = relationship('User', back_populates='history')
+
+    def __init__(self, user, ip):
+        self.ip = ip
+        self.user = user
+        self.time = datetime.datetime.now()
+
+    def __repr__(self):
+        return f'<История {self.user.name}, {self.time}, {self.ip}>'
---
+++ messenger/server/db/repository.py
@@ -0,0 +1,106 @@
+from db.models import Base, User, History
+from settings import DATABASE
+from sqlalchemy import create_engine, exists
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import func
+import datetime
+
+
+class Repository:
+    def __init__(self):
+        self.engine = create_engine(DATABASE, echo=False)
+
+        Base.metadata.create_all(self.engine)
+        Session = sessionmaker(bind=self.engine)
+
+        self.session = Session()
+
+    def user_login(self, user_name, ip):
+        user = self.get_user_by_name(user_name)
+
+        if not user:
+            user = self.add_user(user_name)
+        user.is_online = True
+        user.last_login = datetime.datetime.now()
+        self.add_login_history(user, ip)
+
+    def user_logout(self, user_name):
+        user = self.get_user_by_name(user_name)
+        if user:
+            user.is_online = False
+        self.session.commit()
+
+    def add_user(self, user_name):
+        if not self.session.query(exists().where(User.name == user_name)).scalar():
+            user = User(user_name)
+            self.session.add(user)
+            self.session.commit()
+            return user
+
+    def add_login_history(self, user, ip):
+        history = History(user, ip)
+        self.session.add(history)
+        self.session.commit()
+
+    def get_user_by_name(self, name):
+        user = self.session.query(User).filter(User.name == name)
+        if user.count():
+            return user.first()
+        else:
+            return None
+
+    def users_list(self, active=None):
+        query = self.session.query(User.name)
+        if active is not None:
+            query = query.filter(User.is_online == active)
+        return [value for (value,) in query.all()]
+
+    def active_users_list(self):
+        query = self.session.query(User.name).filter(User.is_online).all()
+        return [value for (value,) in query]
+
+    def login_history(self, user_name=None):
+        query = self.session.query(User.name,
+                                   func.strftime('%Y-%m-%d %H:%M', History.time),
+                                   History.ip).join(User)
+        if user_name:
+            query = query.filter(User.name == user_name)
+        return query.all()
+
+    def get_contact_list(self, user_name):
+        user = self.get_user_by_name(user_name)
+        return [contact.name for contact in user.contacts]
+
+    def add_contact(self, user_name, contact_name):
+        user = self.get_user_by_name(user_name)
+        contact = self.get_user_by_name(contact_name)
+        if contact:
+            user.contacts.append(contact)
+            self.session.add(user)
+            self.session.commit()
+        else:
+            raise Exception
+
+    def remove_contact(self, user_name, contact_name):
+        user = self.get_user_by_name(user_name)
+        contact = self.get_user_by_name(contact_name)
+        if contact:
+            user.contacts.remove(contact)
+            self.session.add(user)
+            self.session.commit()
+        else:
+            raise Exception
+
+
+if __name__ == '__main__':
+    test_db = Repository()
+    test_db.user_login('client_1', '192.168.1.4')
+    test_db.user_login('client_2', '192.168.1.5')
+    test_db.add_contact('client_1', 'client_2')
+    print(test_db.users_list(active=True))
+    test_db.user_logout('client_1')
+    print(test_db.users_list(active=True))
+    print(test_db.login_history('client_1'))
+    print(test_db.users_list())
+    print(test_db.get_contact_list('client_1'))
+    print(test_db.get_contact_list('client_2'))
