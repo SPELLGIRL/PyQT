0157614dc3419eb27dafce72e0e2f6f34cae1962
Antonina <44949556+SPELLGIRL@users.noreply.github.com>
2019-07-25 21:21:23 +0300

    Homework2 (#2)
    Добавлены дескрипторы и метаклассы.

---
+++ messenger/client/descriptors.py
@@ -0,0 +1,17 @@
+import logging
+logger = logging.getLogger('server')
+
+
+class Port:
+    def __set__(self, instance, value):
+        if not 1024 <= value <= 65535:
+            text = f'Попытка запуска с указанием неподходящего ' \
+                f'порта {value}. Допустимы адреса с 1024 до 65535.'
+            logger.critical(text)
+            print(text)
+            exit(1)
+        instance.__dict__[self.name] = value
+
+    def __set_name__(self, owner, name):
+        self.name = name
+
---
+++ messenger/client/metaclasses.py
@@ -0,0 +1,24 @@
+import dis
+
+
+class ClientVerifier(type):
+    def __init__(cls, clsname, bases, clsdict):
+        methods = []
+        for func in clsdict:
+            try:
+                ret = dis.get_instructions(clsdict[func])
+            except TypeError:
+                pass
+            else:
+                for i in ret:
+                    if i.opname == 'LOAD_GLOBAL' or i.opname == 'LOAD_METHOD':
+                        if i.argval not in methods:
+                            methods.append(i.argval)
+        for command in ('accept', 'listen'):
+            if command in methods:
+                raise TypeError('В классе обнаружено использование запрещённого метода')
+        if 'send' in methods or 'receive' in methods:
+            pass
+        else:
+            raise TypeError('Отсутствуют вызовы функций, работающих с сокетами.')
+        super().__init__(clsname, bases, clsdict)
